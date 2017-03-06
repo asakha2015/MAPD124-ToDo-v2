fc641a81a0284487bc9654956feb1e3154d373ee
Andy <andy@andys-mac.local>
2017-03-05 21:27:12 -0500

    Initial Commit

---
+++ anotherSingleView/AddTaskViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddTaskViewController.swift
+//  anotherSingleView
+//
+//  Created by Reza on 2017-03-02.
+//  Copyright © 2017 Reza. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var noteField: UITextField!
+    
+    @IBOutlet weak var isImp: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func btnPressed(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let task = Task(context: context)
+        task.name = textField.text!
+        task.note = noteField.text!
+        task.isImportant = isImp.isOn
+        
+        //Save the data to coredata
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
+    }
+    
+}
---
+++ anotherSingleView/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  anotherSingleView
+//
+//  Created by REza on 2017-03-02.
+//  Copyright © 2017 Reza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    var tasks : [Task] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        //get the data from core data
+        getData()
+        //reload the table view
+        tableView.reloadData()
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        
+        if task.isImportant{
+            cell.textLabel?.text = "📎\(task.name!)"
+            //cell.textLabel?.text = "📎\(task.note!)"
+            
+        } else {
+            
+            cell.textLabel?.text = task.name!
+            //cell.textLabel?.text = task.note!
+        }
+        
+        return cell
+    }
+    
+    func getData() {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            tasks = try context.fetch(Task.fetchRequest())
+        }
+        catch {
+            print("Fetching Failed")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        if editingStyle == .delete {
+            let task = tasks[indexPath.row]
+            context.delete(task)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            do {
+                tasks = try context.fetch(Task.fetchRequest())
+            }
+            catch {
+                print("Fetching Failed")
+            }
+        }
+        tableView.reloadData()
+    }
+
+
+}
+
